66a00fb9469a37ec9b1687056e7d1fdfa86fe617
Oleksii Poliakov <opol2@softserveinc.com>
2022-08-01 14:47:24 +0300

    Initial commit

---
+++ OOD/OOD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
